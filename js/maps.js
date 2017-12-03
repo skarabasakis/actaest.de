1aebf0576803410e49bdad53021e57e07e143ef8
Stelios Karabasakis <skarab@skroutz.gr>
2017-12-03 16:42:36 +0200

    Fix map coordinates

--- js/maps.js
+++ js/maps.js
@@ -1,5 +1,5 @@
 function initMap() {
-  var uluru = {lat: 51.2151827, lng: 6.7855883};
+  var uluru = {lat: 51.2149526, lng: 6.7860385};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: uluru
