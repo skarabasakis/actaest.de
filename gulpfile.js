f96921cb9293694ae44efb6fac55cea0ee34eb00
Stelios Karabasakis <skarab@skroutz.gr>
2018-08-17 17:25:32 +0300

    Add gulp task to concat and minify scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var include = require('gulp-include');
+var minify = require('gulp-minify');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
@@ -9,12 +11,26 @@ gulp.task('sass', function () {
       .pipe(browserSync.stream());
 });
 
-gulp.task('serve', ['sass'], function () {
+gulp.task('scripts', function () {
+  return gulp.src('scripts/main.js')
+      .pipe(include({
+        hardFail: true,
+        includePaths: [
+            __dirname + "/node_modules",
+            __dirname + "/scripts"
+        ]
+      }))
+      .pipe(minify({ext: { min: '.min.js'}}))
+      .pipe(gulp.dest('./js'))
+      .pipe(browserSync.stream());
+});
+gulp.task('serve', ['sass', 'scripts'], function () {
 
   browserSync.init({
     server: "./"
   });
 
   gulp.watch("./scss/style.scss", ['sass']);
+  gulp.watch("./scripts/main.js", ['scripts']);
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+//=require jquery/dist/jquery.js
+//require popper.js/dist/popper.js
+//require bootstrap/dist/js/bootstrap.js
+
+//=include maps.js
