5ff827cada0e248addd607bf40dfe02a0037598d
Stelios Karabasakis <skarab@skroutz.gr>
2018-08-28 10:03:01 +0300

    Move map initialization inline

--- js/main.js
+++ js/main.js
@@ -14309,15 +14309,3 @@ return jQuery;
 })));
 //# sourceMappingURL=bootstrap.js.map
 
-
-function initMap() {
-  var uluru = {lat: 51.2149526, lng: 6.7860385};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
 //=include jquery/dist/jquery.js
 //include popper.js/dist/popper.js
 //=include bootstrap/dist/js/bootstrap.js
-
-//=include maps.js
